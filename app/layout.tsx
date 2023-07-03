6071bfea5915f13c3e2557b2cc26ec2d9377686a
Kunal Khare <khareji594@gmail.com>
2023-07-03 16:04:11 +0530

    Kunal Done 22.08

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,10 @@ import { SiteHeader } from "@/components/site-header"
 
 export const metadata: Metadata = {
   title:siteConfig.name,
-  
+  description:siteConfig.description,
+  icons:{
+    icon:'./favicon.ico'
+  }
 }
 
 interface RootLayoutProps {
@@ -29,9 +32,14 @@ export default function RootLayout({ children }: RootLayoutProps) {
             fontSans.variable
           )}
         >
+          <Providers>
           <div className="relative flex min-h-screen flex-col">
+            <SiteHeader/>
+            <SiteBlob/>
             <div className="flex-1">{children}</div>
+            <SiteFooter/>
           </div>
+          </Providers>
         </body>
       </html>
     </>
--- sanity/lib/client.ts
+++ sanity/lib/client.ts
@@ -7,4 +7,5 @@ export const client = createClient({
   dataset,
   projectId,
   useCdn,
+  token:"skebTqhACXFzz5IZV4OOqbyo960FgpII3ANwa9BeYa70cqDtS6FT7qUh0eM4qj1bx5qWhchvjzsWqixrzexRmdXEK9zcO4cToswCEfow8DthuZY8FNGWGwhpo4yangnqlgNngbsIz5qSI0PQ9CO2apwC5QswZlKYwkVf1MVk3t9NZfaoB8U0",
 })
